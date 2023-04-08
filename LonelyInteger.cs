7340ea57131ce1cd402546a84f8bfe60239201f0
ChurchillSaraswathi <45057335+saraswathiinduraj@users.noreply.github.com>
2023-04-08 17:02:55 +0800

    Add files via upload
    Given an array of integers, where all elements but one occur twice, find the unique element.
    
    Example
    
    The unique element is .
    
    Function Description
    
    Complete the lonelyinteger function in the editor below.
    
    lonelyinteger has the following parameter(s):
    
    int a[n]: an array of integers
    Returns
    
    int: the element that occurs only once
    Input Format
    
    The first line contains a single integer, , the number of integers in the array.
    The second line contains  space-separated integers that describe the values in .
    
    Constraints
    
    It is guaranteed that  is an odd number and that there is one unique element.
    , where .

---
+++ LonelyInteger.cs
@@ -0,0 +1,54 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'lonelyinteger' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int lonelyinteger(List<int> a)
+    {
+        int res = 0;
+        foreach(var item in a)
+        {
+            res = res^item;
+        }
+        return res;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        int result = Result.lonelyinteger(a);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
