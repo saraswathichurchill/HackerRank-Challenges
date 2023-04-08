267a21f48b87dc2e30252164f4086c5b211513ca
ChurchillSaraswathi <45057335+saraswathiinduraj@users.noreply.github.com>
2023-04-08 17:14:02 +0800

    Add files via upload
    Given a time in -hour AM/PM format, convert it to military (24-hour) time.
    
    Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
    - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
    
    Example
    
    
    Return '12:01:00'.
    
    
    Return '00:01:00'.
    
    Function Description
    
    Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
    
    timeConversion has the following parameter(s):
    
    string s: a time in  hour format
    Returns
    
    string: the time in  hour format
    Input Format
    
    A single string  that represents a time in -hour clock format (i.e.:  or ).

---
+++ TimeConversion.cs
@@ -0,0 +1,50 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string timeConversion(string s)
+    {
+       DateTime dateTime = DateTime.ParseExact(s, "hh:mm:sstt",
+           System.Globalization.CultureInfo.InvariantCulture);
+
+            return (dateTime.ToString("HH:mm:ss"));
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = Result.timeConversion(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
