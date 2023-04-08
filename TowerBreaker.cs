01f0b0ae8a462e5d55e9da42650ada7a814cf5e9
ChurchillSaraswathi <45057335+saraswathiinduraj@users.noreply.github.com>
2023-04-08 16:58:38 +0800

    Add files via upload
    Two players are playing a game of Tower Breakers! Player  always moves first, and both players always play optimally.The rules of the game are as follows:
    
    Initially there are  towers.
    Each tower is of height .
    The players move in alternating turns.
    In each turn, a player can choose a tower of height  and reduce its height to , where  and  evenly divides .
    If the current player is unable to make a move, they lose the game.
    Given the values of  and , determine which player will win. If the first player wins, return . Otherwise, return .
    
    Example. 
    
    There are  towers, each  units tall. Player  has a choice of two moves:
    - remove  pieces from a tower to leave  as 
    - remove  pieces to leave 
    
    Let Player  remove . Now the towers are  and  units tall.
    
    Player  matches the move. Now the towers are both  units tall.
    
    Now Player  has only one move.
    
    Player  removes  pieces leaving . Towers are  and  units tall.
    Player  matches again. Towers are both  unit tall.
    
    Player  has no move and loses. Return .
    
    Function Description
    
    Complete the towerBreakers function in the editor below.
    
    towerBreakers has the following paramter(s):
    
    int n: the number of towers
    int m: the height of each tower
    Returns
    
    int: the winner of the game
    Input Format
    
    The first line contains a single integer , the number of test cases.
    Each of the next  lines describes a test case in the form of  space-separated integers,  and .
    
    Constraints
    
    Sample Input
    
    STDIN   Function
    -----   --------
    2       t = 2
    2 2     n = 2, m = 2
    1 4     n = 1, m = 4
    Sample Output
    
    2
    1
    Explanation
    
    We'll refer to player  as  and player  as 
    
    In the first test case,  chooses one of the two towers and reduces it to . Then  reduces the remaining tower to a height of . As both towers now have height ,  cannot make a move so  is the winner.
    
    In the second test case, there is only one tower of height .  can reduce it to a height of either  or .  chooses  as both players always choose optimally. Because  has no possible move,  wins.

---
+++ TowerBreaker.cs
@@ -0,0 +1,65 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'towerBreakers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER m
+     */
+
+    public static int towerBreakers(int n, int m)
+    {
+        if(m==1 || n%2==0)
+        {
+            return 2;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int m = Convert.ToInt32(firstMultipleInput[1]);
+
+            int result = Result.towerBreakers(n, m);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
